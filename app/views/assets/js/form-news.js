fd54bae282f365b961ce54036dd003cce1dfc8d1
Pedro Cardoso <pedropaulo.cardoso@banese.com.br>
2019-08-02 14:07:13 -0300

    js do titulo

--- app/views/assets/js/form-news.js
+++ app/views/assets/js/form-news.js
@@ -1,3 +1,10 @@
-$("#titulo").onkeypress = function() {
-    $(this).style.backgroundColor = "red";
+$(document).ready(function() {   
+
+    var value;
+
+    $(".titulo").keydown(function(e) {
+        console.log("Tecla apertada - "+e.keyCode);
+        console.log("Valor do input - "+$("#titulo").val());
+        console.log("Texto do input - "+$("#titulo").text());
+    });
 });
